ecb39c00c12341944ecc4a07843b5b6c15201370
Alexander Koval <alexander.koval@infrontx.com>
2023-01-05 12:07:28 -0500

    add back button to docs page

--- frontend/src/documentation.tsx
+++ frontend/src/documentation.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BackButton } from './backButton';
+import BackButton from './backButton';
 import MainTemplate from './main-template';
 
 const chapters = [
